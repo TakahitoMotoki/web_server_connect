7818f403a36aeab98cfb9bf70827c82f253c57aa
元木嵩人 <kmnernofkingdeep@gmail.com>
2018-03-14 16:48:40 +0900

    Initial Commit

---
+++ WebServerConnect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebServerConnect
+//
+//  Created by 元木嵩人 on 2018/03/14.
+//  Copyright © 2018年 元木嵩人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
